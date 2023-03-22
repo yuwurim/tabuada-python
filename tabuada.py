8cc920c472f07be2b6e7f52d1100da427326c5f7
Yuri Santos <96179643+yuwurim@users.noreply.github.com>
2023-03-22 08:22:51 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+print('Tabuada!')
+
+n=int(input('Digite o seu número: '))
+x1= n*1
+x2= n*2
+x3= n*3
+x4= n*4
+x5= n*5
+x6= n*6
+x7= n*7
+x8= n*8
+x9= n*9
+x10= n*10
+print('A tabuada de {} é: \n {}x1={}. \n {}x2={}. \n {}x3={}. \n {}x4={}. \n {}x5={}. '.format(n, n, x1, n, x2, n, x3, n, x4, n, x5))
+print(' {}x6={}. \n {}x7={}. \n {}x8={}. \n {}x9={}. \n {}x10={}.'.format(n, x6, n, x7, n, x8, n, x9, n, x10))
